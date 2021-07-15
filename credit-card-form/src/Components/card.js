5b5394b18fef9a0eec33e3681c561e39bd85dccc
siva007-sk <sivapatro12@gmail.com>
2021-07-15 14:48:33 +0530

    completed a submittable app

--- credit-card-form/src/Components/card.js
+++ credit-card-form/src/Components/card.js
@@ -1,38 +1,127 @@
-import chip from "../assets/images/chip.png";
-import visa from "../assets/images/visa.png";
-import visaBackground from "../assets/images/16.jpeg";
-function Card() {
+function Card(props) {
+  const chip =
+    window.location.origin + "/creditCardForm/assets/images/chip.png";
+
+  
   const cardStyle = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${visaBackground})`,
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${setLogo(props.data.cardType).background}")`,
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
   return (
     <div className="card card-container" style={cardStyle}>
-      <div className="card-front">
+      <div
+        className="card-front"
+        style={{ display: `${props.data.show === "front" ? "block" : "none"}` }}
+      >
         <div className="row">
           <img src={chip} alt="chip" className="chip" />
-          <img src={visa} alt="visa" className="card-logo" />
+          <img src={setLogo(props.data.cardType).logo} alt="visa" className="card-logo" />
         </div>
         <div className="row card-number">
-          <p >####</p>
-          <p >####</p>
-          <p >####</p>
-          <p >####</p>
+          {formatCardNumber(props.data.cardNumber).map((numberChunk) => {
+            return <p>{numberChunk}</p>;
+          })}
         </div>
         <div className="name-expiry-container">
           <label className="card-holder-name">
             Card Holder
-            <p>John Snow</p>
+            <p>{`${props.data.cardName?props.data.cardName:"FULL NAME"}`}</p>
           </label>
           <label className="expiry">
             Expires
-            <p>MM/YY</p>
+            <p>{`${props.data.expiryMonth}/${props.data.expiryYear.slice(
+              2,
+              4
+            )}`}</p>
           </label>
         </div>
       </div>
+      <div
+        className="card-back"
+        style={{ display: `${props.data.show === "back" ? "block" : "none"}` }}
+      >
+        <div className="balck-strip"></div>
+        <div style={{ margin: "1em 0em" }}>
+          <label className="cvv-label">cvv</label>
+          <input
+            type="text"
+            style={{ textAlign: "right" }}
+            value={props.data.cvv}
+          />
+        </div>
+        <img src={setLogo(props.data.cardType).logo} alt="visa" className="card-logo-alt" />
+      </div>
     </div>
   );
+}
+function formatCardNumber(number) {
+  const numberSize = number.length;
+  if (numberSize < 16) {
+    for (let i = numberSize; i < 16; i++) {
+      number += "#";
+    }
+  }
+  return [
+    number.slice(0, 4),
+    number.slice(4, 8),
+    number.slice(8, 12),
+    number.slice(12, 16),
+  ];
+}
+function setLogo(cardType) {
+  if (cardType === "AMEX") {
+    return {
+      logo: window.location.origin + "/creditCardForm/assets/images/amex.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/2.jpeg",
+    };
+  } else if (cardType === "DINERSCLUB") {
+    return {
+      logo:
+        window.location.origin + "/creditCardForm/assets/images/dinersclub.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/7.jpeg",
+    };
+  } else if (cardType === "DISCOVER") {
+    return {
+      logo:
+        window.location.origin + "/creditCardForm/assets/images/discover.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/14.jpeg",
+    };
+  } else if (cardType === "JCB") {
+    return {
+      logo: window.location.origin + "/creditCardForm/assets/images/jcb.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/10.jpeg",
+    };
+  } else if (cardType === "MASTERCARD") {
+    return {
+      logo:
+        window.location.origin + "/creditCardForm/assets/images/mastercard.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/9.jpeg",
+    };
+  } else if (cardType === "TROY") {
+    return {
+      logo: window.location.origin + "/creditCardForm/assets/images/troy.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/3.jpeg",
+    };
+  } else if (cardType === "UNIONPAY") {
+    return {
+      logo:
+        window.location.origin + "/creditCardForm/assets/images/unionpay.png",
+      background:
+        window.location.origin + "/creditCardForm/assets/images/4.jpeg",
+    };
+  }
+  return {
+    logo: window.location.origin + "/creditCardForm/assets/images/visa.png",
+    background:
+      window.location.origin + "/creditCardForm/assets/images/16.jpeg",
+  };
 }
 export default Card;
--- credit-card-form/src/Components/creditForm.js
+++ credit-card-form/src/Components/creditForm.js
@@ -1,7 +1,21 @@
+import React from "react";
+import { DEFAULT_CVC_LENGTH, getCardType } from "../utils/cardTypes";
 import Card from "./card";
 
-function CreditForm() {
-  const monthList = [
+class CreditForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardNumber: "",
+      cardName: "",
+      expiryMonth: "MM",
+      expiryYear: "YYYY",
+      cvv: "",
+      cardType: "",
+      show: "front",
+    };
+  }
+  monthList = [
     "01",
     "02",
     "03",
@@ -15,7 +29,7 @@ function CreditForm() {
     "11",
     "12",
   ];
-  const yearList = [
+  yearList = [
     "2019",
     "2020",
     "2021",
@@ -29,58 +43,134 @@ function CreditForm() {
     "2029",
     "2030",
   ];
-  return (
-    <>
-     
-      <div className="form-container card">
-      <Card />
-        <label>
-          Card Number
-          <input type="text" />
-        </label>
-        <label>
-          Card Holders
-          <input type="text" />
-        </label>
-        <span>
-          <label className="expiration-date-conatiner">
-            Experiation Date
-            <div>
-              <select style={{ marginRight: "1em" }}>
-                <option value="00" selected hidden disabled>
-                  Month
-                </option>
-                {monthList.map((month) => {
-                  return (
-                    <option value={month} key={month}>
-                      {month}
-                    </option>
-                  );
-                })}
-              </select>
-              <select>
-                <option value="00" selected hidden>
-                  Year
-                </option>
-                {yearList.map((year) => {
-                  return (
-                    <option value={year} key={year}>
-                      {year}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "cardNumber") {
+      if (value.match(/^[0-9]+$/) || value === "") {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            [name]: value,
+            ["cardType"]: getCardType(value),
+          };
+        });
+      }
+    } else if (name === "cvv") {
+      if (value.match(/^[0-9]+$/) || value === "") {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            [name]: value,
+          };
+        });
+      }
+    } else {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          [name]: value,
+        };
+      });
+    }
+  };
+  handleCvvFocus = (event) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        ["show"]: "back",
+      };
+    });
+  };
+  handleCvvBlur = (event) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        ["show"]: "front",
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="form-container card">
+          <Card data={this.state} />
+          <label>
+            Card Number
+            <input
+              type="text"
+              name="cardNumber"
+              value={this.state.cardNumber}
+              onChange={this.handleChange}
+              maxLength="16"
+            />
           </label>
-          <label className="cvv-container">
-            CVV
-            <input type="text" />
+          <label>
+            Card Holders
+            <input
+              type="text"
+              name="cardName"
+              value={this.state.cardName}
+              onChange={this.handleChange}
+            />
           </label>
-        </span>
-        <button className="btn-active">Submit</button>
-      </div>
-    </>
-  );
+          <span>
+            <label className="expiration-date-conatiner">
+              Experiation Date
+              <div>
+                <select
+                  style={{ marginRight: "1em" }}
+                  name="expiryMonth"
+                  value={this.state.expiryMonth}
+                  onChange={this.handleChange}
+                >
+                  <option value="MM" hidden disabled>
+                    Month
+                  </option>
+                  {this.monthList.map((month) => {
+                    return (
+                      <option value={month} key={month}>
+                        {month}
+                      </option>
+                    );
+                  })}
+                </select>
+                <select
+                  name="expiryYear"
+                  value={this.state.expiryYear}
+                  onChange={this.handleChange}
+                >
+                  <option value="YYYY" disabled hidden>
+                    Year
+                  </option>
+                  {this.yearList.map((year) => {
+                    return (
+                      <option value={year} key={year}>
+                        {year}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+            </label>
+            <label className="cvv-container">
+              CVV
+              <input
+                type="text"
+                name="cvv"
+                value={this.state.cvv}
+                onChange={this.handleChange}
+                maxLength={DEFAULT_CVC_LENGTH}
+                onFocus={this.handleCvvFocus}
+                onBlur={this.handleCvvBlur}
+              />
+            </label>
+          </span>
+          <button className="btn-active">Submit</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default CreditForm;
